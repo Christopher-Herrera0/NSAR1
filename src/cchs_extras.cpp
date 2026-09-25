db7d83d98eb3a0b0b1c9b8ae6fa0f28c324c206f
PandeJoker <cherrera27237@columbushs.com>
2026-09-25 11:03:29 -0400

    update

--- src/cchs_extras.cpp
+++ src/cchs_extras.cpp
@@ -5,11 +5,11 @@ void intertial_check(){
     auto check_motors = [&](std::vector<pros::Motor>& motors) {
         for (auto& m : motors) {
             if (m.get_actual_velocity() > 1){
-
+                printf("[IC]: Robot in motion");
             }
         }
     }
     if (chassis.imu == nullptr || !chassis.imu->is_installed()) {
-        printf("Inertial Check: IMU on port %d not responding\n", chassis.imu != nullptr ? chassis.imu->get_port() : -1);
+        printf("[IC]: IMU on port %d not responding\n", chassis.imu != nullptr ? chassis.imu->get_port() : -1);
     }
 }
